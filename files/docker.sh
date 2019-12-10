2b7b31053744358e66bc579dec38427546d46a62
jwei <jwei@statestreet.com>
2019-12-10 14:57:31 +0800

    initial version

---
+++ files/docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+. /etc/profile
+
+if docker images | grep tomcat ; then
+echo "tomcat is existed and skip pull tomcat"
+else
+docker pull tomcat
+fi
+
+if docker ps -a | grep -i tomcat ; then
+docker rm -f tomcat
+fi
+
+docker run --name tomcat -v ~/pipeline/sample.war:/usr/local/tomcat/webapps/sample.war -dit -p 8081:8080 tomcat
