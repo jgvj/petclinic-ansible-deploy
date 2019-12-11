7939df997a5874cbdbaf87ab932a6ba80ff3c608
jwei <jwei@statestreet.com>
2019-12-11 16:13:58 +0800

    Add jacoco configuration in tomcat container

--- files/docker.sh
+++ files/docker.sh
@@ -12,3 +12,14 @@ docker rm -f tomcat
 fi
 
 docker run --name tomcat -v ~/pipeline/sample.war:/usr/local/tomcat/webapps/sample.war -dit -p 8081:8080 tomcat
+
+#####################################################################################
+# Jacoco related configuration in tomcat container
+# cp jacoco jar to tomcat container
+docker cp ~/pipeline/org.jacoco.agent-0.7.9-runtime.jar tomcat:/usr/local/tomcat/lib/
+
+# cp updated catalina.sh to tomcat container (add jacoco configuration)
+docker cp ~/pipeline/catalina.sh tomcat:/usr/local/tomcat/bin/
+docker exec tomcat chmod 755 bin/catalina.sh
+
+docker restart tomcat
